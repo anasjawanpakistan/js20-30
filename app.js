b82b52103af0cbdb29413a64d4c84f578191fed8
anasjawanpakistan <anasm0213@gmail.com>
2023-09-23 23:17:30 +0500

    first commit

---
+++ app.js
@@ -0,0 +1,16 @@
+// Q1
+
+var userInput = prompt("Enter your name");
+var userInput = userInput.tolowercase;
+
+
+
+var city = ["karachi","lahore"];
+for (var i=0; i< city.length; i++){
+    console.log("match")
+
+        
+    }
+
+
+
